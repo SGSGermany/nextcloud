0b1264ed387fd09f3a8c8719306c6922453c7b20
Daniel Rudolf <github.com@daniel-rudolf.de>
2022-02-19 16:59:44 +0100

    Add copyright header to src/entrypoint.sh

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
+# Nextcloud
+# A php-fpm container of Nextcloud.
+#
+# Copyright (c) 2021  SGS Serious Gaming & Simulations GmbH
+#
+# This work is licensed under the terms of the MIT license.
+# For a copy, see LICENSE file or <https://opensource.org/licenses/MIT>.
+#
+# SPDX-License-Identifier: MIT
+# License-Filename: LICENSE
+
 set -e
 
 [ $# -gt 0 ] || set -- php-fpm "$@"
